be4a55037afbe34036d5f99f80b82835725276e9
guojialin <814183953@qq.com>
2021-07-22 22:20:20 +0800

    修复快速排序bug

--- src/main/kotlin/eg/arithmetic/QSort.kt
+++ src/main/kotlin/eg/arithmetic/QSort.kt
@@ -1,7 +1,7 @@
 package eg.arithmetic
 
 fun QSort(values: IntArray, start: Int, end: Int): IntArray {
-    val piont = values[0]
+    val piont = values[start]
     var i = start
     var j = end
     while (i < j) {
@@ -11,7 +11,7 @@ fun QSort(values: IntArray, start: Int, end: Int): IntArray {
         while ((i < j) and (values[i] < piont)) {
             i++
         }
-        if ((values[i] == values[j]) && (i < j)) {
+        if ((values[i] == values[j]) and (i < j)) {
             i++ // 防止空转
         } else {
             val temp = values[i]
@@ -20,11 +20,11 @@ fun QSort(values: IntArray, start: Int, end: Int): IntArray {
         }
     }
     if (i - 1 > start) QSort(values, start, i - 1)
-    if (j + 1 < end) QSort(values, j + 1, end)
+    if (j + 1 < end)  QSort(values, j + 1, end)
     return values
 }
 
 fun main() {
-    val arr = intArrayOf(3,3,3,7,9,122344,4656,34,34,4656,5,6,7,8,9,343,57765,23,12321)
+    var arr = intArrayOf(3,3,3,7,9,122344,4656,34,34,4656,5,6,7,8,9,343,57765,23,12321)
     QSort(arr,0,arr.size-1).forEach(::println)
 }
