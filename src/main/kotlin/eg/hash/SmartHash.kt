400624d28ab03742a702a357340312c7e351b6ec
guojialin <814183953@qq.com>
2021-07-30 22:42:18 +0800

    hash-无重复元素子串最大长度

--- src/main/kotlin/eg/hash/SmartHash.kt
+++ src/main/kotlin/eg/hash/SmartHash.kt
@@ -17,8 +17,8 @@ fun twoSum(src: Array<Int>, target: Int): Pair<Int?, Int> {
 /**
  * 连续数组
  * 给定一个二维数组，找到0和1数量相等的子数组最大长度
- * 使用sum来维护1，0数量的差值。在for循环中将差值插入hash表中，i为数组下标。当key sum在表中存在，i - hash.get(sum)
- * 表示此次位置出现的与hash.contain()中出现的1、0长度相等
+ * 使用sum来维护1，0数量的差值。在for循环中将差值插入hash表中，i为数组下标。当key sum在表中存在
+ * i - preFix.get(sum)!! 表示此次位置出现的与hash.contain()中出现的1、0长度相等
  */
 fun findMaxLen(src: Array<Int>): Int {
     val preFix = HashMap<Int, Int>()
@@ -39,10 +39,37 @@ fun findMaxLen(src: Array<Int>): Int {
     return max
 }
 
+/**
+ *无重复元素子串最大长度
+ * hash中存储每个元素的位置，设立一个左边界值，当hash中添加过重复元素，移动左边界值到开始出现重复元素位置
+ */
+fun lengthOfMaxLongSubString(str: String): Int {
+    if (str.isNullOrEmpty()) {
+        return 0
+    }
+    val hash = HashMap<Char, Int>()
+    var start = -1
+    var max = Int.MIN_VALUE
+    for (i in str.indices) {
+        if (hash.containsKey(str[i])) {
+            val temp = hash.get(str[i])
+            if (temp!! >= start) {
+                start = temp
+            }
+        }
+        hash.put(str[i], i)
+        max = Math.max(max, i - start)// 计较是否重复添加后i - start 最大距离
+    }
+    return max
+}
+
+
 fun main() {
 //    var num = arrayOf(1, 2, 3, 4, 5)
 //    var (x, y) = twoSum(num, 5)
 //    println("x=$x,y=$y")
-    var num = arrayOf(1,1,0,1,0,0)
-    println(findMaxLen(num))
+//    var num = arrayOf(1, 1, 0, 1, 0, 0)
+//    println(findMaxLen(num))
+    var str = "ABACDEAD"
+    println(lengthOfMaxLongSubString(str))
 }
