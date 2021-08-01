4597b958d28995758e2b7d867be380a18b625a2b
guojialin <814183953@qq.com>
2021-08-01 13:32:21 +0800

    数组-删除指定元素

--- src/main/kotlin/eg/array/SmartArray.kt
+++ src/main/kotlin/eg/array/SmartArray.kt
@@ -30,7 +30,22 @@ fun plusOne(num: Array<Int>): Array<Int> {
     return digits
 }
 
+/**
+ * 删除数组中指定的元素,并返回新数组大小
+ */
+fun delElement(src:Array<Int>?,elem:Int):Int{
+    if (src.isNullOrEmpty()) return -1
+    var index = 0
+    for (i in src.indices){
+        if (src[i] != elem){
+            src[index++] = src[i]
+        }
+    }
+    return index
+}
+
 fun main() {
     var numStr = arrayOf(9,9,9)
-    plusOne(numStr).forEach { println(it) }
+//    plusOne(numStr).forEach { println(it) }
+    println(delElement(numStr,9))
 }
