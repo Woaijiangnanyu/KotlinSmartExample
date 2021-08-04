c9d9e1562b998e1b37f7ca866eee7950d9a2bc30
guojialin <814183953@qq.com>
2021-08-05 00:27:10 +0800

    螺旋矩阵注释

--- src/main/kotlin/eg/matrix/SmartMatrix.kt
+++ src/main/kotlin/eg/matrix/SmartMatrix.kt
@@ -2,44 +2,48 @@ package eg.matrix
 
 fun spiralOrder(src: Array<Array<Int>>): MutableList<Int>? {
     if (src.isEmpty()) return null
-    val arry = mutableListOf<Int>()
-    var up = 0
-    var left = 0
-    var right = src[0].size - 1
-    var down = src.size - 1
-    var x = 0
-    var y = 0
+    val airy = mutableListOf<Int>()
+    var up = 0 // 上边界
+    var left = 0 // 左边界
+    var right = src[0].size - 1 //右边界
+    var down = src.size - 1 //下边界
+    var x = 0  // 初始坐标
+    var y: Int // 初始坐标
     while ((up <= down) && (left <= right)) {
         y = left
+        //向右移动
         while ((y <= right) && boundValid(left, up, right, down)) {
-            arry.add(src[x][y])
+            airy.add(src[x][y])
             y++
         }
         y--
         up++
         x = up
+        //向下移动
         while ((x <= down) && boundValid(left, up, right, down)) {
-            arry.add(src[x][y])
+            airy.add(src[x][y])
             x++
         }
         x--
         right--
         y = right
+        //向左移动
         while ((y >= left) && boundValid(left, up, right, down)) {
-            arry.add(src[x][y])
+            airy.add(src[x][y])
             y--
         }
         y++
         down--
         x = down
+        //向上移动
         while ((x >= up) && boundValid(left, up, right, down)) {
-            arry.add(src[x][y])
+            airy.add(src[x][y])
             x--
         }
         x++
         left++
     }
-    return arry
+    return airy
 }
 
 /**
